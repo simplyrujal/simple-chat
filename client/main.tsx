1b08efc8cabfdfbc94bd45a32e17f4d3d6af5bc1
rujal408 <rujal408@gmail.com>
2026-02-07 11:17:36 +0545

    initial commit

---
+++ client/main.tsx
@@ -0,0 +1,9 @@
+import { Meteor } from "meteor/meteor";
+import { createRoot } from "react-dom/client";
+import { App } from "/imports/ui/App";
+
+Meteor.startup(() => {
+  const container = document.getElementById("react-target");
+  const root = createRoot(container!);
+  root.render(<App />);
+});
