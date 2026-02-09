d0c03e50d993c4890da12fa4a1011b35140da6d4
rujal408 <simplyrujal@gmail.com>
2026-02-09 11:13:41 +0545

    update: add tanstack react query

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AdminRoutes } from './modules/admin/routes';
@@ -5,19 +7,31 @@ import { AuthRoutes } from './modules/auth/routes';
 import { ChatRoutes } from './modules/chat/routes';
 import { RequireAuth } from './shared/components/require-auth';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+    },
+  },
+});
+
 export const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/auth/*" element={<AuthRoutes />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/auth/*" element={<AuthRoutes />} />
 
-        {/* Protected Routes */}
-        <Route element={<RequireAuth />}>
-          <Route path="/admin/*" element={<AdminRoutes />} />
-          <Route path="/*" element={<ChatRoutes />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+          {/* Protected Routes */}
+          <Route element={<RequireAuth />}>
+            <Route path="/admin/*" element={<AdminRoutes />} />
+            <Route path="/*" element={<ChatRoutes />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
