cd376866a9dc6fc4aae1857b9b82ac850a0a1e2c
rujal408 <simplyrujal@gmail.com>
2026-02-13 20:45:47 +0545

    update: added list group of flowbite

--- imports/ui/shared/theme/index.ts
+++ imports/ui/shared/theme/index.ts
@@ -1,8 +1,12 @@
 import { createTheme } from "flowbite-react";
 import button from "./button";
+import dropdown from "./dropdown";
+import listGroup from "./listGroup";
 
 const theme = createTheme({
   button,
+  listGroup,
+  dropdown,
 });
 
 export default theme;
