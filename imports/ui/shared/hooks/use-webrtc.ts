c31db36696654d6a426524020e78a6b91362da32
rujal408 <rujal408@gmail.com>
2026-03-01 09:38:50 +0545

    update: worked well with stun 1

--- imports/ui/shared/hooks/use-webrtc.ts
+++ imports/ui/shared/hooks/use-webrtc.ts
@@ -6,9 +6,9 @@ import {
 
 const ICE_SERVERS: RTCConfiguration = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
+    // { urls: "stun:stun.l.google.com:19302" },
     { urls: "stun:stun1.l.google.com:19302" },
-    { urls: "stun:stun2.l.google.com:19302" },
+    // { urls: "stun:stun2.l.google.com:19302" },
   ],
 };
 
